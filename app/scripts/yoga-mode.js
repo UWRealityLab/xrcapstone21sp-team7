11b51b1066575e0977b7bd6d6d0909f30859b633
Phi Ton <pton@uw.edu>
2021-04-21 05:02:51 +0000

    Initial yoga framework implemented

--- app/scripts/yoga-mode.js
+++ app/scripts/yoga-mode.js
@@ -1,9 +1,20 @@
-// Not actually used, but I'm keeping it here for now
-// as a nice reference for which times go with which images.
-const IMG_ARRAY = ["cherry-blossom.png",
-                   "flat-rock.png"];
 // Time for each image until moving onto next (in milliseconds)
-const TIME_ARRAY = [10000, 15000];
+const TIME_ARRAY = [10000,  // welcome screen (0)
+                    15000,  // headset adjustment (1)
+                    5000,   // breathing exercise (2)
+                    10000,
+                    10000,
+                    5000,
+                    5000,
+                    15000,  // transition to all fours (7)
+                    10000,  // begin cat cow (8)
+                    10000,
+                    25000,
+                    8000,   // transition to high plank (11)
+                    8000,   // transition to low cobra (12)
+                    25000,
+                    10000  // transition to child's pose (14)
+                   ];
 
 /* This code will go through the linked images in the image array, and
  * switch to the next one after the amount of time in the corresponding
@@ -11,6 +22,8 @@ const TIME_ARRAY = [10000, 15000];
  * TODO: draw up yoga images
  *       add yoga mat and emitter button to test
  *       plan out future yoga functionality
+ *       idea: skip to next slide button for yoga?
+ *       play audio script so user doesn't have to look up at instructions
  */
 AFRAME.registerComponent("yoga-mode", {
   init: function() {
@@ -24,10 +37,11 @@ AFRAME.registerComponent("yoga-mode", {
       let timer = TIME_ARRAY[loopCount];
       let timerId = setInterval(countdown, 1000);
       
-      // Initialization for first loop\
-      imagesEl.setAttribute("visible", "true");
+      // Initialization for first loop
+      // imagesEl.setAttribute("visible", "true");
       imagesEl.setAttribute("src", "#yoga-" + loopCount);
       timerEl.setAttribute("visible", "true");
+      el.setAttribute("sound", "src: #meditation-music; autoplay: true; loop: true; positional: false; volume: 0.1");
       imageLoop();
       
       // Recursively goes through images and timers until done
@@ -35,12 +49,12 @@ AFRAME.registerComponent("yoga-mode", {
         setTimeout(function() {
           loopCount++;
           
-          if (loopCount < IMG_ARRAY.length) {
+          if (loopCount < TIME_ARRAY.length) {
             imagesEl.setAttribute("src", "#yoga-" + loopCount);
             timer = TIME_ARRAY[loopCount];
             imageLoop();
           } else { // Clear when done looping
-            imagesEl.setAttribute("visible", "false");
+            // imagesEl.setAttribute("visible", "false");
             timerEl.setAttribute("visible", "false");
             clearInterval(timerId);
           }
