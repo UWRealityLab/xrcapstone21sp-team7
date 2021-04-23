fb90755fc9c0bfab557e19d9e7c32d9eccf9a976
Hristo Ramirez <hristoramirezdiaz@gmail.com>
2021-04-22 23:02:58 -0700

    made menu items simpler to add

--- app/scripts/menu-button.js
+++ app/scripts/menu-button.js
@@ -1,62 +1,137 @@
 // Modified from -> https://github.com/aframevr/aframe/blob/master/examples/showcase/ui/highlight.js
-// cameraRig: { type: "selector", default: "#camRig" },
-    // deviceButtons: { default: ["click", "mousedown", "triggerdown"] },
-    AFRAME.registerComponent('menu-button', {
-      init: function () {
-        let el = this.el;
-        
-        this.onClick = this.onClick.bind(this);
-        this.onMouseEnter = this.onMouseEnter.bind(this);
-        this.onMouseLeave = this.onMouseLeave.bind(this);
-        this.reset = this.reset.bind(this);
-        // backgroundEl.addEventListener('click', this.reset);
-        el.addEventListener('mouseenter', this.onMouseEnter);
-        el.addEventListener('mouseleave', this.onMouseLeave);
-        el.addEventListener('click', this.onClick);
-      },
-      
-      remove: function() {
-        // this.backgroundEl.removeEventListener('click', this.reset);
-        this.el.removeEventListener('mouseenter', this.onMouseEnter);
-        this.el.removeEventListener('mouseleave', this.onMouseLeave);
-        this.el.removeEventListener('click', this.onClick);    
-      },
-    
-      onClick: function (evt) {
-        // evt.target.setAttribute('material', 'color', '#046de7');
-        console.log(`${evt.target.id}-clicked`);
-        this.el.emit(`${evt.target.id}-clicked`, {});
-      },
-    
-      onMouseEnter: function (evt) {
-        evt.target.setAttribute('material', 'color', '#046de7');
-      },
-    
-      onMouseLeave: function (evt) {
-        evt.target.setAttribute('material', 'color', 'white');
-      },
-    
-      reset: function () {
-        this.el.emit('mouseleave');
-      },
-      
-    //   play: function() {
-    //     this.addEventListeners();
-    //   },
-      
-    //   pause: function() {
-    //     this.removeEventListeners();
-    //   },
-      
-    //   addEventListeners: function() {
-    //     this.el.addEventListener('mouseenter', this.onMouseEnter);
-    //     this.el.addEventListener('mouseleave', this.onMouseLeave);
-    //     this.el.addEventListener('click', this.onClick);
-    //   },
-      
-    //   removeEventListeners: function() {
-    //     this.el.removeEventListener('mouseenter', this.onMouseEnter);
-    //     this.el.removeEventListener('mouseleave', this.onMouseLeave);
-    //     this.el.removeEventListener('click', this.onClick);
-    //   }
+
+AFRAME.registerComponent("menu-button", {
+  schema: {
+    title: { type: "string", default: "Title" },
+    info: { type: "string", default: "Info" },
+    img: { type: "selector", default: "#meditation-img" },
+    width: { type: "number", default: 1 },
+  },
+
+  init: function () {
+    let el = this.el;
+    let data = this.data;
+
+    // add image and text
+    let imageEl = document.createElement("a-entity");
+    imageEl.setAttribute("class", "poster");
+    imageEl.setAttribute("material", "src", data.img);
+    imageEl.setAttribute("mixin", "poster");
+    // imageEl.setAttribute("text", {
+    //   value: data.title,
+    //   align: "center",
+    //   shader: "msdf",
+    //   width: data.width,
+    // });
+
+    this.titleText = document.createElement("a-entity");
+    this.titleText.setAttribute("text", {
+      value: data.title,
+      align: "center",
+      shader: "msdf",
+      width: data.width,
     });
+
+    this.infoText = document.createElement("a-entity");
+    this.infoText.setAttribute("mixin", "info-text");
+    this.infoText.setAttribute("text", {
+      value: data.info,
+      align: "center",
+      shader: "msdf",
+      width: 0.8,
+      opacity: 0,
+    });
+
+    // add
+    imageEl.appendChild(this.titleText);
+    imageEl.appendChild(this.infoText);
+    el.appendChild(imageEl);
+
+    this.onClick = this.onClick.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.reset = this.reset.bind(this);
+    // backgroundEl.addEventListener('click', this.reset);
+    el.addEventListener("mouseenter", this.onMouseEnter);
+    el.addEventListener("mouseleave", this.onMouseLeave);
+    el.addEventListener("click", this.onClick);
+  },
+
+  remove: function () {
+    // this.backgroundEl.removeEventListener('click', this.reset);
+    this.el.removeEventListener("mouseenter", this.onMouseEnter);
+    this.el.removeEventListener("mouseleave", this.onMouseLeave);
+    this.el.removeEventListener("click", this.onClick);
+  },
+
+  onClick: function (evt) {
+    console.log(`${evt.target.id}-clicked`);
+    this.el.emit(`${evt.target.id}-clicked`, {});
+  },
+
+  onMouseEnter: function (evt) {
+    // evt.target.setAttribute("material", "color", "#046de7");
+    this.infoText.setAttribute("animation", {
+      property: "material.opacity",
+      to: 0.95,
+      dur: 400,
+      easing: "easeOutExpo",
+    });
+
+    this.infoText.setAttribute("animation__text", {
+      property: "text.opacity",
+      to: 1,
+      dur: 400,
+      easing: "easeOutExpo",
+    });
+  },
+
+  onMouseLeave: function (evt) {
+    // evt.target.setAttribute("material", "color", "white");
+    // evt.target
+    //   .querySelector(".poster")
+    //   .setAttribute("text", {
+    //     value: this.data.title,
+    //     width: this.data.width
+    //   });
+    // evt.target.querySelector(".info-text").
+
+    this.infoText.setAttribute("animation", {
+      property: "material.opacity",
+      to: 0,
+      dur: 200,
+      easing: "easeOutExpo",
+    });
+
+    this.infoText.setAttribute("animation__text", {
+      property: "text.opacity",
+      to: 0,
+      dur: 200,
+      easing: "easeOutExpo"
+    });
+  },
+
+  reset: function () {
+    this.el.emit("mouseleave");
+  },
+
+  //   play: function() {
+  //     this.addEventListeners();
+  //   },
+
+  //   pause: function() {
+  //     this.removeEventListeners();
+  //   },
+
+  //   addEventListeners: function() {
+  //     this.el.addEventListener('mouseenter', this.onMouseEnter);
+  //     this.el.addEventListener('mouseleave', this.onMouseLeave);
+  //     this.el.addEventListener('click', this.onClick);
+  //   },
+
+  //   removeEventListeners: function() {
+  //     this.el.removeEventListener('mouseenter', this.onMouseEnter);
+  //     this.el.removeEventListener('mouseleave', this.onMouseLeave);
+  //     this.el.removeEventListener('click', this.onClick);
+  //   }
+});
--- app/scripts/menu-controls.js
+++ app/scripts/menu-controls.js
@@ -1,98 +1,106 @@
-AFRAME.registerComponent('menu-controls', {
-  init: function() {
+AFRAME.registerComponent("menu-controls", {
+  init: function () {
     let el = this.el;
-  
+
     this.currentMenu;
     // Grab template of menu to display
     this.displayed = false;
-    this.ui = document.querySelector('#activity-menu-template')
-      .content.cloneNode(true).querySelector('#ui');
-    document.querySelector('#camRig').appendChild(this.ui);
-    
+    this.ui = document
+      .querySelector("#activity-menu-template")
+      .content.cloneNode(true)
+      .querySelector("#ui");
+    document.querySelector("#camRig").appendChild(this.ui);
+
     // Event handlers
     this.onMenuActivate = this.onMenuActivate.bind(this);
     this.onMeditationButtonClicked = this.onMeditationButtonClicked.bind(this);
     this.onYogaButtonClicked = this.onYogaButtonClicked.bind(this);
-    
+
     // Button event listeners
-    this.el.addEventListener('abuttondown', this.onMenuActivate); // maybe change trigger button?
-    this.el.sceneEl.addEventListener('meditation-button-clicked', this.onMeditationButtonClicked);
-    this.el.sceneEl.addEventListener('yoga-button-clicked', this.onYogaButtonClicked);
-    
+    this.el.addEventListener("abuttondown", this.onMenuActivate); // maybe change trigger button?
+    this.el.sceneEl.addEventListener(
+      "meditation-button-clicked",
+      this.onMeditationButtonClicked
+    );
+    this.el.sceneEl.addEventListener(
+      "yoga-button-clicked",
+      this.onYogaButtonClicked
+    );
+
     // Helpers
     this.activate = this.activate.bind(this);
     this.deactivate = this.deactivate.bind(this);
   },
-  
+
   /*
     Displays or hides the menu
   */
-  onMenuActivate: function() {
+  onMenuActivate: function () {
     if (this.displayed) {
-      this.ui.setAttribute('visible', 'false');
+      this.ui.setAttribute("visible", "false");
       this.deactivate(this.currentMenu);
     } else {
-      this.el.emit('goMeditation', { returnColor: 'meditation' }); // Darken the environment when menu is displayed
-      this.ui.setAttribute('visible', 'true');``
-      this.activate(document.querySelector('#first-menu'));
+      this.el.emit("goMeditation", { returnColor: "meditation" }); // Darken the environment when menu is displayed
+      this.ui.setAttribute("visible", "true");
+      this.activate(document.querySelector("#first-menu"));
     }
-    
+
     this.displayed = !this.displayed;
   },
-  
+
   /*
     Displays the different meditation options
   */
-  onMeditationButtonClicked: function() {
-    console.log('recieved click');
+  onMeditationButtonClicked: function () {
+    console.log("recieved click");
     // Deactivate first menu options
-    let firstMenu = document.querySelector('#first-menu');
+    let firstMenu = document.querySelector("#first-menu");
     this.deactivate(firstMenu);
-    
+
     // Activate meditation options
-    let medMenu = document.querySelector('#meditation-menu');
+    let medMenu = document.querySelector("#meditation-menu");
     this.activate(medMenu);
   },
-  
+
   /*
     Displays the different yoga options
   */
-  onYogaButtonClicked: function() {},
-  
+  onYogaButtonClicked: function () {},
+
   /*
     Activates menu options so they can be detected by raycasting
   */
-  activate: function(element) {
-    element.querySelectorAll('.option').forEach(option => {
-      option.setAttribute('class', 'rightclickable option');
+  activate: function (element) {
+    element.querySelectorAll(".option").forEach((option) => {
+      option.setAttribute("class", "rightclickable option");
     });
-    element.setAttribute('visible', 'true');
-    
+    element.setAttribute("visible", "true");
+
     // set active menu
     this.currentMenu = element;
   },
-  
+
   /*
     Deactivates menu options so they can't be detected by raycasting
   */
-  deactivate: function(element) {
-    element.setAttribute('visible', 'false');
-    element.querySelectorAll('.option').forEach(option => {
-      option.setAttribute('class', 'option');
+  deactivate: function (element) {
+    element.setAttribute("visible", "false");
+    element.querySelectorAll(".option").forEach((option) => {
+      option.setAttribute("class", "option");
     });
-    
+
     this.currentMenu = null;
   },
-  
-  remove: function() {},
 
-//   addEventListeners: function() {
-//     this.el.addEventListener('meditation-button-clicked', this.onMeditationButtonClicked);
-//     this.el.addEventListener('yoga-button-clicked', this.onYogaButtonClicked);
-//   },
-  
-//   removeEventListeners: function() {
-//     this.el.removeEventListener('meditation-button-clicked', this.onMeditationButtonClicked);
-//     this.el.removeEventListener('yoga-button-clicked', this.on)
-//   },
+  remove: function () {},
+
+  //   addEventListeners: function() {
+  //     this.el.addEventListener('meditation-button-clicked', this.onMeditationButtonClicked);
+  //     this.el.addEventListener('yoga-button-clicked', this.onYogaButtonClicked);
+  //   },
+
+  //   removeEventListeners: function() {
+  //     this.el.removeEventListener('meditation-button-clicked', this.onMeditationButtonClicked);
+  //     this.el.removeEventListener('yoga-button-clicked', this.on)
+  //   },
 });
