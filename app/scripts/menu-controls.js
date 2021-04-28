23da0820e41f192b8b6d3b4edcd66569e789eafa
Hristo Ramirez <hristoramirezdiaz@gmail.com>
2021-04-27 22:53:27 -0700

    volume buttons to sliders, doesnt match intial volume

--- app/scripts/menu-controls.js
+++ app/scripts/menu-controls.js
@@ -13,6 +13,7 @@ AFRAME.registerComponent("menu-controls", {
 
     // Event handlers
     this.onMenuActivate = this.onMenuActivate.bind(this);
+    this.onVolumeChanged = this.onVolumeChanged.bind(this);
     this.onMeditationButtonClicked = this.onMeditationButtonClicked.bind(this);
     this.onYogaButtonClicked = this.onYogaButtonClicked.bind(this);
 
@@ -24,7 +25,8 @@ AFRAME.registerComponent("menu-controls", {
     this.removeEventListeners = this.removeEventListeners.bind(this);
 
     // Button event listeners
-    this.el.addEventListener("abuttondown", this.onMenuActivate); // maybe change trigger button?
+    // TODO: change trigger button
+    this.el.addEventListener("abuttondown", this.onMenuActivate);
     this.el.sceneEl.addEventListener(
       "meditation-button-clicked",
       this.onMeditationButtonClicked
@@ -33,31 +35,37 @@ AFRAME.registerComponent("menu-controls", {
       "yoga-button-clicked",
       this.onYogaButtonClicked
     );
+    this.el.sceneEl.addEventListener(
+      "guided-meditation-button-clicked",
+      this.onGuidedMeditationClicked
+    );
+    this.el.sceneEl.addEventListener(
+      "story-telling-button-clicked",
+      this.onStoryMeditationClicked
+    );
+    this.el.sceneEl.addEventListener(
+      "confidence-booster-button-clicked",
+      this.onConfidenceBoosterClicked
+    );
+    this.el.sceneEl.addEventListener(
+      "breathing-exercise-button-clicked",
+      this.onBreathingExerciseButtonClicked
+    );
+    this.el.sceneEl.addEventListener("volume-slider-changed", this.onVolumeChanged);
 
-    this.el.sceneEl.addEventListener(
-      'guided-meditation-button-clicked', this.onGuidedMeditationClicked
-    );
-    this.el.sceneEl.addEventListener(
-      'story-telling-button-clicked', this.onStoryMeditationClicked
-    );
-    this.el.sceneEl.addEventListener(
-      'confidence-booster-button-clicked', this.onConfidenceBoosterClicked
-    );
-    this.el.sceneEl.addEventListener(
-      'breathing-exercise-button-clicked', this.onBreathingExerciseButtonClicked
-    );
+    // this.el.sceneEl.addEventListener(
+    //   'up-volume-clicked', this.upVolume
+    // );
 
-    this.el.sceneEl.addEventListener(
-      'up-volume-clicked', this.upVolume
-    );
-
-    this.el.sceneEl.addEventListener(
-      'down-volume-clicked', this.downVolume
-    );
+    // this.el.sceneEl.addEventListener(
+    //   'down-volume-clicked', this.downVolume
+    // );
 
     // Helpers
     this.activate = this.activate.bind(this);
     this.deactivate = this.deactivate.bind(this);
+    this.activateSliders = this.activateSliders.bind(this);
+    this.deactivateSliders = this.deactivateSliders.bind(this);
   },
 
   /*
@@ -67,12 +75,16 @@ AFRAME.registerComponent("menu-controls", {
     if (this.displayed) {
       this.ui.setAttribute("visible", "false");
       this.deactivate(this.currentMenu);
-      this.deactivate(document.querySelector("#volume-button"));
+      // this.deactivate(document.querySelector("#volume-button"));
+      this.deactivateSliders();
     } else {
       this.el.emit("goMeditation", { returnColor: "meditation" }); // Darken the environment when menu is displayed
       this.ui.setAttribute("visible", "true");
-      this.activate(document.querySelector("#volume-button"));
+      // this.activate(document.querySelector("#volume-button"));
       this.activate(document.querySelector("#first-menu"));
+
+      // Turn on sliders
+      this.activateSliders();
     }
 
     this.displayed = !this.displayed;
@@ -92,45 +104,45 @@ AFRAME.registerComponent("menu-controls", {
   },
 
   // Start the meditation script
-  onGuidedMeditationClicked: function() {
+  onGuidedMeditationClicked: function () {
     let sky = document.querySelector("#sky");
     // sound="on: model-loaded; src: #meditation-1; autoplay: true; loop: false; positional: false; volume: 1";
-    let attr = sky.getAttribute('sound');
+    let attr = sky.getAttribute("sound");
     attr.src = "#meditation-1";
-      
-    sky.setAttribute('sound', attr);
+
+    sky.setAttribute("sound", attr);
   },
 
   // Start the rain story script
-  onStoryMeditationClicked: function() {
+  onStoryMeditationClicked: function () {
     let sky = document.querySelector("#sky");
     // sound="on: model-loaded; src: #rain; autoplay: true; loop: false; positional: false; volume: 1";
-    let attr = sky.getAttribute('sound');
-    attr.src = "#rain";  
+    let attr = sky.getAttribute("sound");
+    attr.src = "#rain";
 
-    sky.setAttribute('sound', attr);
+    sky.setAttribute("sound", attr);
   },
 
   // Start confidence booster script
-  onConfidenceBoosterClicked: function() {
+  onConfidenceBoosterClicked: function () {
     let sky = document.querySelector("#sky");
     // sound="on: model-loaded; src: #confidence-meditation; autoplay: true; loop: false; positional: false; volume: 1";
-    let attr = sky.getAttribute('sound');
-    attr.src = "#confidence-meditation";  
+    let attr = sky.getAttribute("sound");
+    attr.src = "#confidence-meditation";
 
-    sky.setAttribute('sound', attr);
+    sky.setAttribute("sound", attr);
   },
 
   /**
    * Start breathing exercise.
    */
-  onBreathingExerciseButtonClicked: function() {
+  onBreathingExerciseButtonClicked: function () {
     // TODO
     // let sky = document.querySelector('#sky');
     // sound = 'on: model-loaded; src: #breath-exercise-meditation; autoplay: true; loop: false; positional: false; volume: 1';
     // sky.setAttribute('sound', sound);
 
-    this.el.sceneEl.emit('breath-capture-start');
+    this.el.sceneEl.emit("breath-capture-start");
   },
 
   /*
@@ -146,34 +158,65 @@ AFRAME.registerComponent("menu-controls", {
     this.activate(yogaMenu);
   },
 
-  upVolume: function () {
-
-    let sky = document.querySelector('#sky');
-    let attr = sky.getAttribute('sound');
+  onVolumeChanged: function (evt) {
+    let sky = document.querySelector("#sky");
+    let attr = sky.getAttribute("sound");
     console.log("Volume before:" + attr.volume);
-    attr.volume = attr.volume + 0.05;
+    attr.volume = evt.detail.percent;
     console.log("Volume after:" + attr.volume);
-    
-    sky.setAttribute('sound', attr);
-        
+
+    sky.setAttribute("sound", attr);
   },
 
-  downVolume: function () {
+  // upVolume: function () {
 
-    let sky = document.querySelector('#sky');
+  //   let sky = document.querySelector('#sky');
+  //   let attr = sky.getAttribute('sound');
+  //   console.log("Volume before:" + attr.volume);
+  //   attr.volume = attr.volume + 0.05;
+  //   console.log("Volume after:" + attr.volume);
 
-    let attr = sky.getAttribute('sound');
-    console.log("Volume before:" + attr.volume);
+  //   sky.setAttribute('sound', attr);
 
-    attr.volume = attr.volume - 0.05;
+  // },
 
-    if (attr.volume < 0) {
-      attr.volume = 0;
-    }
-    console.log("Volume after:" + attr.volume);
-    
-    sky.setAttribute('sound', attr);
-        
+  // downVolume: function () {
+
+  //   let sky = document.querySelector('#sky');
+
+  //   let attr = sky.getAttribute('sound');
+  //   console.log("Volume before:" + attr.volume);
+
+  //   attr.volume = attr.volume - 0.05;
+
+  //   if (attr.volume < 0) {
+  //     attr.volume = 0;
+  //   }
+  //   console.log("Volume after:" + attr.volume);
+
+  //   sky.setAttribute('sound', attr);
+  // },
+
+  /*
+    Turn on sliders
+  */
+  activateSliders: function () {
+    this.ui.querySelectorAll(".slider").forEach((slider) => {
+      slider.setAttribute("visible", true);
+      slider
+        .querySelector(".container")
+        .setAttribute("class", "rightclickable container");
+    });
+  },
+
+  /*
+    Turn off sliders
+  */
+  deactivateSliders: function () {
+    this.ui.querySelectorAll(".slider").forEach((slider) => {
+      slider.setAttribute("visible", false);
+      slider.querySelector(".container").setAttribute("class", "container");
+    });
   },
 
   /*
@@ -210,12 +253,12 @@ AFRAME.registerComponent("menu-controls", {
   //     this.el.addEventListener('yoga-button-clicked', this.onYogaButtonClicked);
   //   },
 
-  removeEventListeners: function() {
+  removeEventListeners: function () {
     let el = this.el;
 
-    el.removeEventListener('abuttondown', this.onMenuActivate); // maybe change trigger button?
+    el.removeEventListener("abuttondown", this.onMenuActivate); // maybe change trigger button?
     el.sceneEl.removeEventListener(
-      'meditation-button-clicked',
+      "meditation-button-clicked",
       this.onMeditationButtonClicked
     );
     el.sceneEl.removeEventListener(
@@ -224,16 +267,21 @@ AFRAME.registerComponent("menu-controls", {
     );
 
     el.sceneEl.removeEventListener(
-      'guided-meditation-button-clicked', this.onGuidedMeditationClicked
+      "guided-meditation-button-clicked",
+      this.onGuidedMeditationClicked
     );
     el.sceneEl.removeEventListener(
-      'story-telling-button-clicked', this.onStoryMeditationClicked
+      "story-telling-button-clicked",
+      this.onStoryMeditationClicked
     );
     el.sceneEl.removeEventListener(
-      'confidence-booster-button-clicked', this.onConfidenceBoosterClicked
+      "confidence-booster-button-clicked",
+      this.onConfidenceBoosterClicked
     );
     el.sceneEl.removeEventListener(
-      'breathing-exercise-button-clicked', this.onBreathingExerciseButtonClicked
+      "breathing-exercise-button-clicked",
+      this.onBreathingExerciseButtonClicked
     );
+    this.el.sceneEl.removeEventListener("volume-slider-changed", this.onVolumeChanged);
   },
 });
---
+++ app/scripts/slider.js
@@ -0,0 +1,112 @@
+// Needs to activated by the menu
+AFRAME.registerComponent("slider", {
+  schema: {
+    title: { type: "string", default: ""}
+  },
+  init: function () {
+    let el = this.el;
+    let data = this.data;
+
+    // Dimensions
+    this.barWidth = 1;
+
+    // Text
+    this.text = document.createElement("a-entity");
+    this.text.setAttribute("position", "0 0.07 0");
+    this.text.setAttribute("text", {
+      value: data.title,
+      align: "left",
+      shader: "msdf",
+    });
+
+    // Container
+    this.container = document.createElement("a-entity");
+    this.container.setAttribute("class", "container");
+    this.container.setAttribute(
+      "geometry",
+      `primitive: plane; width: ${this.barWidth}; height: 0.1;`
+    );
+    this.container.setAttribute(
+      "material",
+      "shader: flat; opacity: 0"
+    );
+
+    // Line
+    this.line = document.createElement("a-entity");
+    this.line.setAttribute(
+      "geometry",
+      `primitive: box; width: ${this.barWidth}; height: 0.01; depth: 0.01`
+    );
+    this.line.setAttribute(
+      "material",
+      "shader: flat; opacity: 1; color: white; side: front"
+    );
+
+    // Circle
+    this.marker = document.createElement("a-entity");
+    this.marker.setAttribute(
+      "geometry",
+      "primitive: cylinder; radius: 0.02; height: 0.01"
+    );
+    this.marker.setAttribute("rotation", "90 0 0");
+    this.marker.setAttribute(
+      "material",
+      "shader: flat; opacity: 1; side: double; color: white"
+    );
+    
+    el.appendChild(this.text);
+    el.appendChild(this.line);
+    el.appendChild(this.marker);
+    el.appendChild(this.container);
+
+    // Handlers
+    this.onClick = this.onClick.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+
+    // Listeners
+    this.container.addEventListener("click", this.onClick);
+    this.container.addEventListener("mouseenter", this.onMouseEnter);
+    this.container.addEventListener("mouseleave", this.onMouseLeave);
+  },
+
+  remove: function () {
+    this.container.removeEventListener("click", this.onClick);
+    this.container.removeEventListener("mouseenter", this.onMouseEnter);
+    this.container.removeEventListener("mouseleave", this.onMouseLeave);
+  },
+
+  onClick: function (evt) {
+    const x = evt.detail.intersection.point.x;
+    const percent = ( x + 0.5 * this.barWidth ) / this.barWidth;
+    console.log(percent);
+
+    this.marker.setAttribute("animation", {
+      property: "position.x",
+      to: x,
+      dur: 500,
+      easing: "easeOutExpo"
+    });
+
+    this.el.sceneEl.emit(`${this.el.id}-changed`, { percent: percent});
+    console.log(`${this.el.id}-changed`);
+  },
+
+  onMouseEnter: function() {
+    this.marker.setAttribute("animation", {
+      property: "geometry.radius",
+      to: 0.03,
+      dur: 500,
+      easing: "easeOutExpo"
+    });
+  },
+
+  onMouseLeave: function() {
+    this.marker.setAttribute("animation", {
+      property: "geometry.radius",
+      to: 0.02,
+      dur: 500,
+      easing: "easeOutExpo"
+    });
+  },
+});
