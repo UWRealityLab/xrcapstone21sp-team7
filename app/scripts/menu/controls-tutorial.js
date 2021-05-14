ef5c8cf161b5c8f1c13b58536fafd637d41fe864
Hristo Ramirez <hristoramirezdiaz@gmail.com>
2021-05-13 21:03:02 -0700

    added teleportation instructions

--- app/scripts/menu/controls-tutorial.js
+++ app/scripts/menu/controls-tutorial.js
@@ -1,3 +1,7 @@
+const HEADER_TEXT_WIDTH = 14;
+const TITLE_TEXT_WIDTH = 8;
+const INFO_TEXT_WIDTH = 4;
+
 AFRAME.registerComponent("controls-tutorial", {
   init: function() {
     let el = this.el;
@@ -10,7 +14,7 @@ AFRAME.registerComponent("controls-tutorial", {
     welcome.setAttribute("text", {
       value: "Welcome to Zendin!",
       align: "center",
-      width: 14,
+      width: HEADER_TEXT_WIDTH,
       shader: "msdf",
     });
     welcome.setAttribute("position", "0 7 1.75");
@@ -21,46 +25,80 @@ AFRAME.registerComponent("controls-tutorial", {
     */
 
     // menu
+    const menuContainer = document.createElement("a-entity");
+    menuContainer.setAttribute("position", "0 3.5 1.6");
+    menuContainer.setAttribute("rotation", "0 180 0");
+
     const menuInstructions = "TODO: menu instructions";
     const menu = document.createElement("a-entity");
     menu.setAttribute("text", {
       value: menuInstructions,
       align: "center",
       shader: "msdf",
-      width: 8
+      width: TITLE_TEXT_WIDTH
     });
-    menu.setAttribute("position", "0 4 1.6");
-    menu.setAttribute("rotation", "0 180 0");
+
+    menuContainer.append(menu);
+    //
 
     // locomotion
-    const teleportInstructions = "you can get around the garden\nby walking or using the left\njoystick to teleport";
-    const teleport = document.createElement("a-entity");
-    teleport.setAttribute("text", {
-      value: teleportInstructions,
+    const locomotionContainer = document.createElement("a-entity");
+    locomotionContainer.setAttribute("position", "3.48 3.5 -0.8");
+    locomotionContainer.setAttribute("rotation", "0 -90 0");
+
+    const gettingAround = document.createElement("a-entity");
+    gettingAround.setAttribute("text", {
+      value: "Getting Around",
       align: "center",
       shader: "msdf",
-      width: 8
+      width: TITLE_TEXT_WIDTH
     });
-    teleport.setAttribute("position", "3.8 4 -0.85");
-    teleport.setAttribute("rotation", "0 -90 0");
+    gettingAround.setAttribute("position", "0 1.4 0");
+
+    const teleportGif = document.createElement("a-entity");
+    teleportGif.setAttribute("geometry", "primitive: plane");
+    teleportGif.setAttribute("material", {
+      shader: "gif",
+      src: "#teleport-gif"
+    });
+    teleportGif.setAttribute("scale", "2 2 2");
+
+    const teleportInsructions = document.createElement("a-entity");
+    teleportInsructions.setAttribute("text", {
+      value: "Flick and hold the joystick\nforward to select your destination",
+      align: "center",
+      shader: "msdf",
+      width: INFO_TEXT_WIDTH
+    });
+    teleportInsructions.setAttribute("position", "0 -1.4 0");
+
+    locomotionContainer.appendChild(gettingAround);
+    locomotionContainer.appendChild(teleportGif);
+    locomotionContainer.appendChild(teleportInsructions);
+    //
 
     // asset placement
+    const placementContainer = document.createElement("a-entity");
+    placementContainer.setAttribute("position", "-3.8 3.5 -0.5");
+    placementContainer.setAttribute("rotation", "0 90 0");
+
     const placementInstructions = "TODO: placement instructions";
     const placement = document.createElement("a-entity");
     placement.setAttribute("text", {
       value: placementInstructions,
       align: "center",
       shader: "msdf",
-      width: 8
+      width: TITLE_TEXT_WIDTH
     });
-    placement.setAttribute("position", "-3.8 4 -0.85");
-    placement.setAttribute("rotation", "0 90 0");
+
+    placementContainer.appendChild(placement);
+    //
 
     // Add to building
     building.appendChild(welcome);
-    building.appendChild(menu);
-    building.appendChild(teleport);
-    building.appendChild(placement);
+    building.appendChild(menuContainer);
+    building.appendChild(locomotionContainer);
+    building.appendChild(placementContainer);
   },
   remove: function() {},
 })
