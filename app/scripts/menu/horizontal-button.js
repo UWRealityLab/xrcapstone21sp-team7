f83d139bd6f18a46fe60d9b56fcd34f881d4306e
jthi32 <jthi32@cs.washington.edu>
2021-05-17 12:57:27 -0700

    Added Volume to display

--- app/scripts/menu/horizontal-button.js
+++ app/scripts/menu/horizontal-button.js
@@ -3,8 +3,9 @@
 //         -- almost entirely same code, just the events emitted are different.
 AFRAME.registerComponent("horizontal-button", {
     schema: {
-      title_1: { type: "string", default: ""},
-      title_2: { type: "string", default: ""},
+      title_1: { type: "string", default: "" },
+      title_2: { type: "string", default: "" },
+      title_3: { type: "string", default: "" },
       img: { type: "selector", default: "#meditation-img" }
     },
     init: function () {
@@ -42,12 +43,33 @@ AFRAME.registerComponent("horizontal-button", {
         color: "#B7B4A7",
       });
 
-      // Custom Text
+      // Custom Text 2
       this.custom_2 = document.createElement("a-entity");
       this.custom_2.setAttribute("class", "title-2");
       this.custom_2.setAttribute("position", "0.3 0 0");
       this.custom_2.setAttribute("text", {
         value: "TEST_2",
+        align: "center",
+        shader: "msdf",
+        color: "#B7B4A7",
+      });
+
+      // Third Title
+      this.text_3 = document.createElement("a-entity");
+      this.text_3.setAttribute("position", "0.15, -0.2, 0");
+      this.text_3.setAttribute("text", {
+        value: data.title_3,
+        align: "left",
+        shader: "msdf",
+        color: "#B7B4A7",
+      });
+
+      // Custom Text 3
+      this.custom_3 = document.createElement("a-entity");
+      this.custom_3.setAttribute("class", "title-3");
+      this.custom_3.setAttribute("position", "0.3 -0.2 0");
+      this.custom_3.setAttribute("text", {
+        value: "TEST_3",
         align: "center",
         shader: "msdf",
         color: "#B7B4A7",
@@ -74,6 +96,8 @@ AFRAME.registerComponent("horizontal-button", {
       this.container.appendChild(this.custom_1);
       this.container.appendChild(this.text_2);
       this.container.appendChild(this.custom_2);
+      this.container.appendChild(this.text_3);
+      this.container.appendChild(this.custom_3);
       //this.container.appendChild(this.soundImage);
       el.appendChild(this.container);
   
