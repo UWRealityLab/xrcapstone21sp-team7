446a9ffd141a4fc7f73c7c0f48fbb9935763a26c
Hristo Ramirez <hristoramirezdiaz@gmail.com>
2021-05-19 18:06:05 -0700

    moved controls image inside building and removed button

--- app/scripts/menu/controls-tutorial.js
+++ app/scripts/menu/controls-tutorial.js
@@ -1,12 +1,13 @@
 // Text Dimensions
 const HEADER_TEXT_WIDTH = 10;
-const TITLE_TEXT_WIDTH = 4;
+const TITLE_TEXT_WIDTH = 3.5;
 const INFO_TEXT_WIDTH = 1.5;
 
 // Positions & Offsets
 const CENTER_X = 2.5;
 const CENTER_Y = 3.5;
 const CENTER_Z = -1;
+const OFFSET_Z = 1.7;
 
 AFRAME.registerComponent("controls-tutorial", {
   init: function () {
@@ -17,7 +18,7 @@ AFRAME.registerComponent("controls-tutorial", {
     // State
     this.visible = true;
 
-    // Hides controls tutorial once now longer inside building
+    // Hides controls tutorial once no longer inside building
     this.toggleVisibility = (evt) => {
       const newPosition = evt.detail.newPosition;
 
@@ -91,42 +92,52 @@ AFRAME.registerComponent("controls-tutorial", {
     // Controls image and remove buttonw
     const controls = document.createElement("a-image");
     controls.setAttribute("id", "controls-tutorial-img");
+    controls.setAttribute("class", "controls-container");
     controls.setAttribute("src", "#controls-img");
-    controls.setAttribute("position", "0 3 8");
-    controls.setAttribute("rotation", "-10 180 0");
+    controls.setAttribute("position", {
+      x: -CENTER_X,
+      y: CENTER_Y,
+      z: CENTER_Z
+    });
+    controls.setAttribute("rotation", "0 90 0");
     controls.setAttribute("width", "4");
     controls.setAttribute("height", "1.75");
 
-    const controlsButton = document.createElement("a-box");
-    controlsButton.setAttribute("id", "controls-tutorial-dismiss");
-    controlsButton.setAttribute("class", "rightclickable");
-    controlsButton.setAttribute("position", "0 1.75 7.625");
-    controlsButton.setAttribute("scale", "0.6 0.6 0.2");
-    controlsButton.setAttribute("rotation", "5 0 0");
-    controlsButton.setAttribute("color", "white");
-    controlsButton.setAttribute("yoga-button", "");
+    // const controlsButton = document.createElement("a-box");
+    // controlsButton.setAttribute("id", "controls-tutorial-dismiss");
+    // controlsButton.setAttribute("class", "rightclickable");
+    // controlsButton.setAttribute("position", "0 1.75 7.625");
+    // controlsButton.setAttribute("scale", "0.6 0.6 0.2");
+    // controlsButton.setAttribute("rotation", "5 0 0");
+    // controlsButton.setAttribute("color", "white");
+    // controlsButton.setAttribute("yoga-button", "");
 
-    const controlsButtonImg = document.createElement("a-image");
-    controlsButtonImg.setAttribute("id", "controls-tutorial-dismiss-img");
-    controlsButtonImg.setAttribute("src", "#controls-next-img");
-    controlsButtonImg.setAttribute("position", "0 1.75 7.5");
-    controlsButtonImg.setAttribute("rotation", "-5 180 0");
-    controlsButtonImg.setAttribute("width", "0.5");
-    controlsButtonImg.setAttribute("height", "0.5");
+    // const controlsButtonImg = document.createElement("a-image");
+    // controlsButtonImg.setAttribute("id", "controls-tutorial-dismiss-img");
+    // controlsButtonImg.setAttribute("src", "#controls-next-img");
+    // controlsButtonImg.setAttribute("position", "0 1.75 7.5");
+    // controlsButtonImg.setAttribute("rotation", "-5 180 0");
+    // controlsButtonImg.setAttribute("width", "0.5");
+    // controlsButtonImg.setAttribute("height", "0.5");
 
 
-    /* Instructions 
-      TODO: probably use images of some sort to avoid using text
-    */
+    /* Instructions */
 
     // menu
     const menuContainer = document.createElement("a-entity");
-    menuContainer.setAttribute("position", `0 ${CENTER_Y} 1.6`);
-    menuContainer.setAttribute("rotation", "0 180 0");
+    menuContainer.setAttribute("position", {
+      x: CENTER_X,
+      y: CENTER_Y,
+      z: CENTER_Z + OFFSET_Z,
+    });
+    menuContainer.setAttribute("rotation", "0 -90 0");
 
-    const menu = createText("TODO: menu instructions", INFO_TEXT_WIDTH);
+    const navigateTheMenu = createText("Navigate the Menu", TITLE_TEXT_WIDTH);
+    const menuInstructions = createText("TODO: Menu Instructions", INFO_TEXT_WIDTH);
 
-    menuContainer.append(menu);
+
+    menuContainer.append(navigateTheMenu);
+    menuContainer.append(menuInstructions);
     //
 
     // locomotion
@@ -134,7 +145,7 @@ AFRAME.registerComponent("controls-tutorial", {
     locomotionContainer.setAttribute("position", {
       x: CENTER_X,
       y: CENTER_Y,
-      z: CENTER_Z - 1.5,
+      z: CENTER_Z - OFFSET_Z,
     });
     locomotionContainer.setAttribute("rotation", "0 -90 0");
 
@@ -153,11 +164,11 @@ AFRAME.registerComponent("controls-tutorial", {
     // asset placement
     const placementContainer = document.createElement("a-entity");
     placementContainer.setAttribute("position", {
-      x: -CENTER_X,
+      x: CENTER_X,
       y: CENTER_Y,
-      z: CENTER_Z - 1.5,
+      z: CENTER_Z,
     });
-    placementContainer.setAttribute("rotation", "0 90 0");
+    placementContainer.setAttribute("rotation", "0 -90 0");
 
     const customizeYourGarden = createText(
       "Customize Your Garden",
@@ -183,8 +194,8 @@ AFRAME.registerComponent("controls-tutorial", {
     this.building.appendChild(welcome);
 
     this.building.appendChild(controls);
-    this.building.appendChild(controlsButton);
-    this.building.appendChild(controlsButtonImg);
+    // this.building.appendChild(controlsButton);
+    // this.building.appendChild(controlsButtonImg);
 
     this.building.appendChild(menuContainer);
     this.building.appendChild(locomotionContainer);
@@ -199,7 +210,7 @@ AFRAME.registerComponent("controls-tutorial", {
     this.el.sceneEl.removeEventListener("controls-tutorial-dismiss-triggered", this.dismissControls);
   },
 
-  dismissControls: function() {
-    document.querySelectorAll('[id^="controls-tutorial"]').forEach((control) => control.setAttribute("visible", "false"));
-  }
+  // dismissControls: function() {
+  //   document.querySelectorAll('[id^="controls-tutorial"]').forEach((control) => control.setAttribute("visible", "false"));
+  // }
 });
