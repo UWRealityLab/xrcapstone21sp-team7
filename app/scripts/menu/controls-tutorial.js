7ba1db12592208dba41608282e02940289c5e7b4
Hristo Ramirez <hristoramirezdiaz@gmail.com>
2021-05-31 15:49:41 -0700

    combined gates/building to one object

--- app/scripts/menu/controls-tutorial.js
+++ app/scripts/menu/controls-tutorial.js
@@ -4,16 +4,15 @@ const TITLE_TEXT_WIDTH = 3.5;
 const INFO_TEXT_WIDTH = 1.5;
 
 // Positions & Offsets
-const CENTER_X = 2.5;
+const CENTER_X = 28.5;
 const CENTER_Y = 3.4;
-const CENTER_Z = -1;
-const OFFSET_Z = 1.7;
+const CENTER_Z = 3.7;
+const OFFSET_X = 1.7;
 
 AFRAME.registerComponent("controls-tutorial", {
   init: function () {
-    let el = this.el;
     // grab building to place all instructions relative to the building
-    this.building = document.querySelector("#entrance-building");
+    let el = this.building = this.el;
 
     // State
     this.visible = true;
@@ -25,10 +24,10 @@ AFRAME.registerComponent("controls-tutorial", {
       // note: maybe check position since generate-base-garden
       // can make the garden dynamic
       const newVisibility =
-        19 < newPosition.x &&
-        newPosition.x < 26.5 &&
-        -5 < newPosition.z &&
-        newPosition.z < 5;
+        25 < newPosition.x &&
+        newPosition.x < 32 &&
+        -4.4 < newPosition.z &&
+        newPosition.z < 4.4;
 
       if (this.visible != newVisibility) {
         this.building.querySelectorAll(".gif-panel").forEach((gifPanel) => {
@@ -76,59 +75,41 @@ AFRAME.registerComponent("controls-tutorial", {
     };
     /* */
 
-    // welcome message
-    const welcome = document.createElement("a-entity");
-    welcome.setAttribute("text", {
-      value: "Welcome to Zendin!",
-      align: "center",
-      width: HEADER_TEXT_WIDTH,
-      shader: "msdf",
-      color: "#555555",
-    });
-    welcome.setAttribute("position", "0 6 1.75");
-    welcome.setAttribute("rotation", "20 180 0");
+    // // welcome message
+    // const welcome = document.createElement("a-entity");
+    // welcome.setAttribute("text", {
+    //   value: "Welcome to Zendin!",
+    //   align: "center",
+    //   width: HEADER_TEXT_WIDTH,
+    //   shader: "msdf",
+    //   color: "#555555",
+    // });
+    // welcome.setAttribute("position", "0 6 1.75");
+    // welcome.setAttribute("rotation", "20 180 0");
 
-    // Controls image and remove buttonw
+    // // Controls image and remove button
     const controls = document.createElement("a-image");
     controls.setAttribute("id", "controls-tutorial-img");
     controls.setAttribute("class", "control-container");
     controls.setAttribute("src", "#controls-img");
     controls.setAttribute("position", {
-      x: -CENTER_X - 1,
+      x: CENTER_X,
       y: CENTER_Y,
-      z: CENTER_Z,
+      z: -CENTER_Z,
     });
-    controls.setAttribute("rotation", "0 90 0");
     controls.setAttribute("width", "4");
     controls.setAttribute("height", "1.75");
-
-    // const controlsButton = document.createElement("a-box");
-    // controlsButton.setAttribute("id", "controls-tutorial-dismiss");
-    // controlsButton.setAttribute("class", "rightclickable");
-    // controlsButton.setAttribute("position", "0 1.75 7.625");
-    // controlsButton.setAttribute("scale", "0.6 0.6 0.2");
-    // controlsButton.setAttribute("rotation", "5 0 0");
-    // controlsButton.setAttribute("color", "white");
-    // controlsButton.setAttribute("yoga-button", "");
-
-    // const controlsButtonImg = document.createElement("a-image");
-    // controlsButtonImg.setAttribute("id", "controls-tutorial-dismiss-img");
-    // controlsButtonImg.setAttribute("src", "#controls-next-img");
-    // controlsButtonImg.setAttribute("position", "0 1.75 7.5");
-    // controlsButtonImg.setAttribute("rotation", "-5 180 0");
-    // controlsButtonImg.setAttribute("width", "0.5");
-    // controlsButtonImg.setAttribute("height", "0.5");
 
     /* Instructions */
 
     // menu
     const menuContainer = document.createElement("a-entity");
     menuContainer.setAttribute("position", {
-      x: CENTER_X,
+      x: CENTER_X - OFFSET_X,
       y: CENTER_Y,
-      z: CENTER_Z + OFFSET_Z,
+      z: CENTER_Z,
     });
-    menuContainer.setAttribute("rotation", "0 -90 0");
+    menuContainer.setAttribute("rotation", "0 -180 0");
 
     const navigateTheMenu = createText("Navigate the Menu", TITLE_TEXT_WIDTH);
     const menuGif = createGif("#menu-gif");
@@ -145,11 +126,11 @@ AFRAME.registerComponent("controls-tutorial", {
     // locomotion
     const locomotionContainer = document.createElement("a-entity");
     locomotionContainer.setAttribute("position", {
-      x: CENTER_X,
+      x: CENTER_X + OFFSET_X,
       y: CENTER_Y,
-      z: CENTER_Z - OFFSET_Z,
+      z: CENTER_Z,
     });
-    locomotionContainer.setAttribute("rotation", "0 -90 0");
+    locomotionContainer.setAttribute("rotation", "0 -180 0");
 
     const gettingAround = createText("Getting Around", TITLE_TEXT_WIDTH);
     const teleportGif = createGif("#teleport-gif");
@@ -170,7 +151,7 @@ AFRAME.registerComponent("controls-tutorial", {
       y: CENTER_Y,
       z: CENTER_Z,
     });
-    placementContainer.setAttribute("rotation", "0 -90 0");
+    placementContainer.setAttribute("rotation", "0 -180 0");
 
     const customizeYourGarden = createText(
       "Customize Your Garden",
@@ -188,37 +169,21 @@ AFRAME.registerComponent("controls-tutorial", {
     //
 
     // Add to building
-    welcome.setAttribute("class", "control-container");
+    // welcome.setAttribute("class", "control-container");
     menuContainer.setAttribute("class", "control-container");
     locomotionContainer.setAttribute("class", "control-container");
     placementContainer.setAttribute("class", "control-container");
 
-    this.building.appendChild(welcome);
-
+    // this.building.appendChild(welcome);
     this.building.appendChild(controls);
-    // this.building.appendChild(controlsButton);
-    // this.building.appendChild(controlsButtonImg);
-
     this.building.appendChild(menuContainer);
     this.building.appendChild(locomotionContainer);
     this.building.appendChild(placementContainer);
 
     this.el.sceneEl.addEventListener("teleported", this.toggleVisibility);
-    // this.el.sceneEl.addEventListener(
-    //   "controls-tutorial-dismiss-triggered",
-    //   this.dismissControls
-    // );
   },
 
   remove: function () {
     this.el.sceneEl.removeEventListener("teleported", this.toggleVisibility);
-    // this.el.sceneEl.removeEventListener(
-    //   "controls-tutorial-dismiss-triggered",
-    //   this.dismissControls
-    // );
   },
-
-  // dismissControls: function() {
-  //   document.querySelectorAll('[id^="controls-tutorial"]').forEach((control) => control.setAttribute("visible", "false"));
-  // }
 });
